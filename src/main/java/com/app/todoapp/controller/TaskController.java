7f6fb9744ce0c8a85c679d89724f395359d91c4f
Anurag319 <anuragupadhyay319@gmail.com>
2025-12-14 17:59:01 +0530

    install lombok + save title through UI

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -2,10 +2,10 @@ package com.app.todoapp.controller;
 
 import com.app.todoapp.models.Task;
 import com.app.todoapp.services.TaskService;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +34,12 @@ public class TaskController {
         model.addAttribute("tasks",tasks);
         return "tasks"; // this is a html page
         // view is going to be a thymeleaf template
+    }
+
+    @PostMapping
+    // The @RequestParam annotation in Spring Boot is used to extract query parameters from a URL (e.g., ?name=John) or form data from an HTTP request and bind them to method parameters in a controller.
+    public String createTask(@RequestParam String title){
+        taskService.createTask(title);
+        return "redirect:/tasks"; // redirect page to tasks
     }
 }
